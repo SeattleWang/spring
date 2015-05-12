fee4f9ced3f7c2866545130328c0ec6786cb9606
wangsong <wans@shininghub.cn>
2015-05-12 15:27:07 +0800

    update user

--- src/main/java/com/shininghub/seattle/User.java
+++ src/main/java/com/shininghub/seattle/User.java
@@ -1,5 +1,15 @@
 package com.shininghub.seattle;
 
 public class User {
+	private Integer id;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
 }
